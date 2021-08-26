0bb07768ac699b523a7131d072e992ab7c69fb22
mkurfeyiz <m.kurfeyiz98@gmail.com>
2021-08-26 14:39:10 +0300

    Initial commit.

---
+++ src/test/java/com/mkurfeyiz/ecommerce_web/ECommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkurfeyiz.ecommerce_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
