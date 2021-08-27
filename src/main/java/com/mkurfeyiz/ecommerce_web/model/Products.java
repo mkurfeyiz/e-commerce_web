85d293460ce960bd971b523317669cc308200cb3
mkurfeyiz <m.kurfeyiz98@gmail.com>
2021-08-27 10:25:27 +0300

    Initial commit of products and users.

--- src/main/java/com/mkurfeyiz/ecommerce_web/model/Products.java
+++ src/main/java/com/mkurfeyiz/ecommerce_web/model/Products.java
@@ -1,4 +1,83 @@
 package com.mkurfeyiz.ecommerce_web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Products {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "Price")
+    private double price;
+    @Column(name = "Gender")
+    private String gender;
+    @Column(name = "Category")
+    private String category;
+    @Column(name = "Brand")
+    private String brand;
+
+    public Products(int id, String name, double price, String gender, String category, String brand) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.gender = gender;
+        this.category = category;
+        this.brand = brand;
+    }
+
+    public Products() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
--- src/main/java/com/mkurfeyiz/ecommerce_web/model/Users.java
+++ src/main/java/com/mkurfeyiz/ecommerce_web/model/Users.java
@@ -1,4 +1,83 @@
 package com.mkurfeyiz.ecommerce_web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "firstName")
+    private String fname;
+    @Column(name = "lastName")
+    private String lname;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    public Users(){
+
+    }
+
+    public Users(int id, String fname, String lname, String phone, String email, String password) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
